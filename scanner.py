3d518c422aa124362aa70ea053a2963b42e327c3
Zsolt Imre <imrexzsolt@gmail.com>
2019-04-08 23:28:06 +0100

    Updates to make scanner much more reliable.

--- scanner.py
+++ scanner.py
@@ -1,4 +1,3 @@
-import time
 import socket
 import threading
 
@@ -12,44 +11,45 @@ class Scanner(threading.Thread):
     def __checksum(self, message):
         if (type(message).__name__ != 'str'):
             message = message.decode('ascii')
+            print(message)
         lbytes = list(map(ord, message))
         return hex(sum(lbytes) % 256)[2:].encode('ascii')
 
-    def __receive(self, sock):
-        data = None
-        sock.settimeout(1)
+    def __read_response(self, sock):
+        """
+        Socket-level read of remote stub response.
+        """
+        d = b''
+        while True:
+            r_byte = sock.recv(1)
+            if r_byte is None or len(r_byte) == 0 or r_byte == 0:
+                break
+            d += r_byte
+            if r_byte == b'#' and d[::-1] != b'\\':
+                d += sock.recv(2)
+                return d
+        return None
+
+    def __receive(self, sock, timeout=False):
+        """
+        Receive data from remote stub.
+
+            :returns Response: Data received
+        """
         try:
-            data = sock.recv(4096)
+            if timeout is True:
+                sock.settimeout(2)
+            rdata = self.__read_response(sock)
+            if timeout is True:
+                sock.settimeout(None)
+            return rdata
         except socket.timeout:
             return None
-        sock.settimeout(None)
-        return data
 
     def __send(self, sock, command):
         chk = self.__checksum(command)
-        command = bytes(command, 'ascii')
-        to_send = b'+$' + command + b'#' + chk
+        to_send = b'$' + bytes(command, 'ascii') + b'#' + chk
         sock.send(to_send)
-        return self.__receive(sock)
-
-    def __probe(self, sock):
-        commands = [
-            'QStartNoAckMode',  # No need for acknowledgement
-            '!',                # If we found a server, let's not make it 
-                                # exit once we disconnect
-            'qSupported'        # Ask supported features
-        ]
-        for command in commands:
-            data = self.__send(sock, command)
-            time.sleep(1)
-
-        sock.close()
-        if data is None:
-            return False
-        print(data)
-        if 'PacketSize' not in data.decode('ascii'):
-            return False
-        return True
 
     def __connect(self, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -61,9 +61,35 @@ class Scanner(threading.Thread):
         s.settimeout(None)
         return s
 
+    def __probe(self, port):
+        sock = self.__connect(port)
+        if sock is None:
+            return False
+
+        commands = [
+            'QStartNoAckMode',  # No need for acknowledgement
+            '!',                # If we found a server, let's not make it 
+                                # exit once we disconnect
+            'qSupported'        # Ask supported features
+        ]
+
+        for command in commands:
+            self.__send(sock, command)
+            data = self.__receive(sock, timeout=True)
+
+        if data is None:
+            sock.close()
+            return
+
+        if 'PacketSize' in data.decode('ascii'):
+            print("\n[GDB Server @ %s:%d]: Capabilities: %s" % (
+                self.address,
+                port,
+                data.decode('ascii'))
+            )
+
+        sock.close()
+
     def run(self):
         for port in self.ports:
-            sock = self.__connect(port)
-            if sock is None or self.__probe(sock) is False:
-                continue
-            print("\nFound gdbserver on address %s:%d" % (self.address, port))
+            self.__probe(port)
